e0a142f36b4cd3575e1ab125c241e59d7fd4c091
Döll, Sebastian <sebastian.doell@axelspringer.de>
2017-06-02 09:16:57 +0200

    ~ removing legacy code

--- asse-action-onlyadmin.php
+++ asse-action-onlyadmin.php
@@ -1,26 +1,20 @@
 <?php
 
-// @codingStandardsIgnoreFile
+defined( 'ABSPATH' ) || exit;
 
-if (defined('ASSE_ONLY_ADMIN_CAN_LOGIN') && ASSE_ONLY_ADMIN_CAN_LOGIN === true) {
-    add_action('admin_init', 'redirect_non_admin_users');
-}
-/**
- * Redirect non-admin users to home page
- *
- * This function is attached to the 'admin_init' action hook.
- */
-function redirect_non_admin_users() {
-    if ( ! current_user_can( 'manage_options' ) && '/wp-admin/admin-ajax.php' != $_SERVER['PHP_SELF'] ) {
-        wp_redirect( home_url() . "/hagen-maintenance.html");
-        exit;
+Class Asse_Sitemap {
+
+    public function __construct() {
+        add_action( 'init', array( $this, 'remove_thickbox_on_frontend' ) );
     }
+
+    public function remove_thickbox_on_frontend() {
+        if ( ! is_admin() ) {
+            wp_deregister_style('thickbox');
+            wp_deregister_script('thickbox');
+        }
+    }
+
 }
 
-function remove_thickbox_on_frontend() {
-    if (!is_admin()) {
-        wp_deregister_style('thickbox');
-        wp_deregister_script('thickbox');
-    }
-}
-add_action('init', 'remove_thickbox_on_frontend');
+$asse_sitemap = new Asse_Sitemap();
